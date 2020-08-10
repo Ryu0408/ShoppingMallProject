04658ca03ce2899c3a92b49167b10c0ed5c8310f
Ryu0408 <fbtmdghks11@naver.com>
2020-08-11 00:01:53 +0900

    22-1. UserInfoChangeController 구현완료
    (2020-08-11 AM 00:01)

--- ShoppingMollProject/src/main/java/com/rsh/smp/controller/UsersInfoChangeController.java
+++ ShoppingMollProject/src/main/java/com/rsh/smp/controller/UsersInfoChangeController.java
@@ -1,13 +1,16 @@
 package com.rsh.smp.controller;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.rsh.smp.service.JoinService;
 import com.rsh.smp.service.LoginService;
 import com.rsh.smp.vo.UsersVO;
 
@@ -15,6 +18,11 @@ import com.rsh.smp.vo.UsersVO;
 public class UsersInfoChangeController {
 	
 	@Autowired LoginService loginService;
+	
+	@Inject BCryptPasswordEncoder pwdEncoder;
+	
+	@Autowired JoinService joinService;
+
 
 	@RequestMapping(value = "/userinfochange/", method = RequestMethod.GET)
 	public String login(HttpSession session, Model model) {
@@ -27,5 +35,18 @@ public class UsersInfoChangeController {
 		UsersVO usersVO = loginService.selectGetUsers("id",usersSession);
 		model.addAttribute(usersVO);
 		return "userInfoChange";
+	}
+	
+	@RequestMapping(value = "/join/updateusers", method = RequestMethod.POST)
+	public String joinInsert(UsersVO usersVO, Model model) {
+		String inputPass = usersVO.getPassword();
+		String pwd = pwdEncoder.encode(inputPass);
+		usersVO.setPassword(pwd);
+		if(usersVO.getAgreeEmailCheck() == null) {usersVO.setAgreeEmailCheck("off");}
+		if(usersVO.getAgreeSmsCheck() == null) {usersVO.setAgreeSmsCheck("off");}
+		joinService.updateUsers(usersVO);
+		model.addAttribute("alertContent","회원정보가 수정되었습니다^^");
+		model.addAttribute("path","mypage/");
+		return "alert";
 	}
 }
